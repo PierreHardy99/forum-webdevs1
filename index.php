351ea256194d305bdda97f8c67c39e9ac86860d4
Pierre Hardy <pierre.hardy1999@gmail.com>
2022-03-22 18:58:53 +0100

    modification structure

--- index.php
+++ index.php
@@ -1,27 +1,9 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="src/css/style.css">
-    <link rel="stylesheet" href="src/css/accueil.css">
-    <title>Forum Webdev1</title>
-</head>
-<body>
-    <header>
-        <nav>
-            <div id="forumLogo">
-                <a href="#"><img src="./src/img/" alt="Logo"></a>
-            </div>
-            <ul>
-                <li><a href="#">Lorem</a></li>
-                <li><a href="#">ipsum</a></li>
-                <li><a href="#">indolor</a></li>
-                <li><a href="#">login</a></li>
-            </ul>
-        </nav>
-    </header>
-    <main>
+<?php 
+    $titre = "Forum Webdev1";
+    require_once './src/common/template.php';
+?>
+
+<main>
         <div class="navLeft">
             <ul>
                 <li><a href="#">test 01</a></li>
@@ -30,9 +12,8 @@
                 <li><a href="#">test 04</a></li>
             </ul>
         </div>
-    </main>
-    <footer>
+</main>
 
-    </footer>
-</body>
-</html>
+<?php 
+    require_once './src/common/footer.php';
+?>
---
+++ src/common/nav.php
@@ -0,0 +1,13 @@
+<header>
+        <nav>
+            <div id="forumLogo">
+                <a href="#"><img src="./src/img/" alt="Logo"></a>
+            </div>
+            <ul>
+                <li><a href="#">Lorem</a></li>
+                <li><a href="#">ipsum</a></li>
+                <li><a href="#">indolor</a></li>
+                <li><a href="#">login</a></li>
+            </ul>
+        </nav>
+</header>
