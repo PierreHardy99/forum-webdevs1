76b6a22b20fb265137c721fa1be1201545b066a7
Pierre Hardy <pierre.hardy1999@gmail.com>
2022-03-23 09:37:24 +0100

    Modification de la nav

--- src/common/nav.php
+++ src/common/nav.php
@@ -1,13 +1,13 @@
 <header>
         <div id="forumLogo">
-            <a href="#"><img src="./src/img/" alt="Logo"></a>
+            <a href="./index.php"><img src="./src/img/" alt="Logo"></a>
         </div>
         <nav>
             <ul>
                 <li><a href="#">Lorem</a></li>
                 <li><a href="#">ipsum</a></li>
                 <li><a href="#">indolor</a></li>
-                <li><a href="#">login</a></li>
+                <li><a href="./login.php">login</a></li>
             </ul>
         </nav>
 </header>
